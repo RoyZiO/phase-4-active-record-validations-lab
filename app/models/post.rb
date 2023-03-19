e03c6f70ecc7c813770917e156146ee4534a6404
RoyZiO <nziokinroy@gmail.com>
2023-03-20 01:25:46 +0300

    completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :disallow_title
+
+    #Custom validation
+    def disallow_title
+        if title == "True Facts"
+            errors.add(:title, "title already in use")
+        end
+    end
+    
 end
+
